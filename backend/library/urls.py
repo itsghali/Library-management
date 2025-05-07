34b882d8c11fbfa2e17b2acdc2f46bd37ec750d1
itsghali <ghali.alami.louati@gmail.com>
2025-05-07 12:37:29 +0100

    commit changes

--- backend/library/urls.py
+++ backend/library/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('books/', BookListCreateView.as_view(), name='book-list'),
     path('borrow/', BorrowListCreateView.as_view(), name='borrow-list'),
     path('reservation/', ReservationListCreateView.as_view(), name='reservation-list'),
-
     path('', BookListCreateView.as_view(), name='api-root'),
 ]
